68b2c9710919d92c7368054d85c8d5b76ed38b1b
Andy Cao <andy@boardpro.com>
2023-10-03 21:54:54 +1300

    feat: add progress bar

--- components/quiz/quiz-player2.tsx
+++ components/quiz/quiz-player2.tsx
@@ -72,8 +72,8 @@ const QuizPlayer: React.FC<QuizPlayerProps> = ({
   const currentQuiz = quizData[currentQuizIndex];
 
   return (
-    <div>
-      <h2>Quiz Player</h2>
+    <div className="w-screen h-full flex-1 flex flex-col bg-slate-50">
+      <h2 className="right-10 top-10">Quiz @ Emojiu.cc</h2>
       {showOver ? (
         <>
           <h1>Game over</h1>
@@ -81,22 +81,71 @@ const QuizPlayer: React.FC<QuizPlayerProps> = ({
         </>
       ) : (
         <>
+          <h1 className="text-4xl">Number:{currentQuizIndex + 1}</h1>
           {showQuestion && (
-            <div>
-              <p>Category: {currentQuiz.category}</p>
-              <p>Question: {currentQuiz.question}</p>
-              <p>Countdown: {countdown} seconds</p>
-            </div>
+            <QuestionView
+              question={currentQuiz.question}
+              percentage={countdown / questionDurationInSeconds}
+            />
           )}
           {showAnswer && (
-            <div>
-              <p>Category: {currentQuiz.category}</p>
-              <p>Answer: {currentQuiz.answer}</p>
-              <p>Countdown: {countdown} seconds</p>
-            </div>
+            <>
+              <AnswerView answer={currentQuiz.answer}></AnswerView>
+              <p>
+                Countdown: {countdown}/{answerDurationInSeconds} seconds
+              </p>
+            </>
           )}
         </>
       )}
+    </div>
+  );
+};
+
+const QuestionView = ({
+  question,
+  percentage,
+}: {
+  question: string;
+  percentage: number;
+}) => {
+  return (
+    <div className="w-full flex-1 flex flex-col gap-9 justify-center items-center bg-slate-100">
+      <p className="text-3xl">{question}</p>
+      <p>{percentage * 100}% seconds</p>
+      <ProgressBar progress={percentage} />
+    </div>
+  );
+};
+
+const AnswerView = ({ answer }: { answer: string }) => {
+  return (
+    <div className="w-full flex-1 flex flex-col gap-9 justify-center items-center bg-slate-100">
+      <p className="text-3xl">Answer: {answer}</p>
+    </div>
+  );
+};
+
+// src/components/ProgressBar.tsx
+// import React from 'react';
+// import classNames from 'classnames';
+
+interface ProgressBarProps {
+  progress: number; // Progress percentage (0-100)
+  color?: string; // Custom color (optional)
+}
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ progress, color }) => {
+  const progressBarStyle = {
+    width: `${progress * 100}%`,
+  };
+
+  return (
+    <div className="w-[300px] rounded-xl border-yellow-300 border-4">
+      <div
+        className="h-9 rounded-lg bg-gray-300"
+        style={progressBarStyle}
+      ></div>
     </div>
   );
 };
